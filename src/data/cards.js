9838ffa3bc4a390f4b9da8dc9609bce30a50ddaa
hossam255 <hossam255@gmail.com>
2022-10-02 22:29:01 +0200

    added build files

--- src/data/cards.js
+++ src/data/cards.js
@@ -26,32 +26,42 @@ const cards = [
   },
   {
     accountName :"focus",
-    image:"https://picsum.photos/800/900",
+    image:"https://picsum.photos/700/800",
     storyBorder: true,
     likedByText: "amaar",
-    likedByNumber: 89,
-    hours: 1,
+    likedByNumber: 80,
+    hours: 9,
     comments: [
       {
         user: "hossam",
         text: "Amazing content, keep it up!",
         id: 4,
       },
+      {
+        user: "mezo",
+        text: "Niceeeee!",
+        id: 3,
+      }
     ],
   },
   {
     accountName :"focus",
-    image:"https://picsum.photos/800/900",
+    image:"https://picsum.photos/900",
     storyBorder: true,
     likedByText: "amaar",
-    likedByNumber: 89,
-    hours: 1,
+    likedByNumber: 111,
+    hours: 3,
     comments: [
       {
         user: "malek",
         text: "Love this!",
         id: 5,
       },
+      {
+        user: "mostafa",
+        text: "Like!",
+        id: 2,
+      }
     ]
   }
 
